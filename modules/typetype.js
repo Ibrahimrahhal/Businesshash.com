09acc68890e175cfc84440568036ddc86766f03d
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2018-10-08 07:28:15 -0700

    Text animation added using typetype plugin

---
+++ modules/typetype.js
@@ -0,0 +1,12 @@
+$('.main-hero-text')
+  .typetype('تذّوق طعّم الإحترافيّة في ريّادة الأعمال',
+  {
+    e: 0,
+    t: 100,
+    keypress: function (){
+    },
+    callback: function (){
+
+    }
+  }
+);
