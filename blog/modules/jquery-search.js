33e5c3e3c813812fccf43393d88f33b148ebe352
ibrahimrahhal <ibrahim.rahhal3636@gmai.com>
2018-10-17 12:26:40 -0700

    blog tamplate

---
+++ blog/modules/jquery-search.js
@@ -0,0 +1,18 @@
+$(function () {
+$(".header-nav-searchbar-search").on("focus",function () {
+  $(".header-nav-searchbar-submit").toggleClass("header-nav-searchbar-submit-hover");
+
+
+
+
+});
+$(".header-nav-searchbar-search").on("blur",function () {
+  $(".header-nav-searchbar-submit").toggleClass("header-nav-searchbar-submit-hover");
+
+
+
+
+});
+
+
+});
