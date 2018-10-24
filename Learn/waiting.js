5d54977e1bc1c0f5352b68be1ddad19410f99b0b
ibrahimrahhal <ibrahim.rahhal3636@gmai.com>
2018-10-24 09:41:24 -0700

    learn place holders

---
+++ Learn/waiting.js
@@ -0,0 +1,12 @@
+$(function(){
+$(".main-wrapper-submit").click(function(){
+$(".main-wrapper-email-form").show("fade");
+// $(".main-wrapper-email-form").css('display': 'flex');
+$(".black").show("blind");
+
+
+});
+
+
+
+});
