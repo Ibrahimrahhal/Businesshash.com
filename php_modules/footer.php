816e2b90cd6508b2ec8ce8466414b1927dedfece
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2018-11-18 12:51:51 -0800

    global css

---
+++ php_modules/footer.php
@@ -0,0 +1,25 @@
+<footer>
+  <div class="page-four-footer-upper">
+    <div class="page-four-footer-upper-left">
+<img src="../assets/Logoheader.svg" class="page-four-footer-upper-left-logo" />
+<div class="page-four-footer-upper-left-copy">
+2019&#169; copyright
+</div>
+    </div>
+
+
+  </div>
+  <hr style="width:40%; transform:translateY(-2px);" />
+  <div class="page-four-footer-bottom">
+
+<div class="page-four-footer-bottom-icons">
+<a class="page-four-footer-bottom-icon icon-up" href="#"><i class="fa fab fa-facebook icon"></i></a>
+<a class="page-four-footer-bottom-icon icon-up" href="#"><i class="fa fab fa-linkedin icon"></i></a>
+<a class="page-four-footer-bottom-icon icon-up" href="#"><i class="fa fa-pinterest-p icon"></i></a>
+</div>
+<div class="page-four-footer-bottom-text">
+تابعنا على شبكة التواصل المفضلة لديك
+</div>
+  </div>
+
+</footer>
