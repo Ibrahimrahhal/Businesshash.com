886f4112fd78e46799692b72bcdee4fcaed6ad03
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2018-11-20 11:36:26 -0800

    added auto prefixer and gulp-clean css

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,34 @@ var importCss = require("gulp-cssimport");
 var postcss      = require('gulp-postcss');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
+var autoprefixBrowsers = ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'];
 
 function cssloop(paths){
 paths.forEach((path)=>{
   gulp.src(__dirname+path.input)
       .pipe(importCss({skipComments:false}))
+        .pipe(autoprefixer({
+          cascade: false,
+          flexbox : true,
+          grid :true,
+          browsers: autoprefixBrowsers
+        })).pipe(cleanCSS())
     //   .pipe(cleanCSS({debug: true}, (details) => {
     //   console.log(`${details.name}: ${details.stats.originalSize}`);
     //   console.log(`${details.name}: ${details.stats.minifiedSize}`);
     // }))
+
       .pipe(gulp.dest(__dirname+path.output));
+    // gulp.src(__dirname+"/flex.css")
+    //
+    //
+    //
+    //     //   .pipe(cleanCSS({debug: true}, (details) => {
+    //     //   console.log(`${details.name}: ${details.stats.originalSize}`);
+    //     //   console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    //     // }))
+    //       .pipe(gulp.dest(__dirname+"/doon"));
+
 
 //
 // gulp.src(__dirname+path.input)
